1563a0f354a08db9943ccbb4ad3a85f190f3525e
Santiago Arce <santiago.arce@uao.edu.co>
2025-03-24 18:48:06 -0500

    fix: user

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.models import db, Usuario
+from app.models import db, User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 users_bp = Blueprint('users', __name__)
@@ -7,27 +7,27 @@ users_bp = Blueprint('users', __name__)
 @users_bp.route('/register', methods=['POST'])
 def register():
     data = request.json
-    if Usuario.query.filter_by(email=data['email']).first():
+    if User.query.filter_by(email=data['email']).first():
         return jsonify({"error": "Email ya registrado"}), 400
 
-    nuevo_usuario = Usuario(
+    nuevo_User = User(
         nombre=data['nombre'],
         email=data['email']
     )
-    nuevo_usuario.set_password(data['contraseña'])
-    db.session.add(nuevo_usuario)
+    nuevo_User.set_password(data['contraseña'])
+    db.session.add(nuevo_User)
     db.session.commit()
 
-    return jsonify({"mensaje": "Usuario registrado correctamente"}), 201
+    return jsonify({"mensaje": "User registrado correctamente"}), 201
 
 
 @users_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
-    usuario = Usuario.query.filter_by(email=data['email']).first()
+    User = User.query.filter_by(email=data['email']).first()
 
-    if usuario and usuario.check_password(data['contraseña']):
-        token = create_access_token(identity=usuario.id)
+    if User and User.check_password(data['contraseña']):
+        token = create_access_token(identity=User.id)
         return jsonify({"token": token}), 200
 
     return jsonify({"error": "Credenciales inválidas"}), 401
@@ -36,9 +36,9 @@ def login():
 @users_bp.route('/perfil', methods=['GET'])
 @jwt_required()
 def perfil():
-    usuario_id = get_jwt_identity()
-    usuario = Usuario.query.get(usuario_id)
+    User_id = get_jwt_identity()
+    User = User.query.get(User_id)
     return jsonify({
-        "nombre": usuario.nombre,
-        "email": usuario.email
+        "nombre": User.nombre,
+        "email": User.email
     })
