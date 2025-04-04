4f685f3d10aabbd37a9c96c068edc9410f83309b
Santiago Arce <santiago.arce@uao.edu.co>
2025-04-03 23:17:52 -0500

    FIX: ENV

--- app/config.py
+++ app/config.py
@@ -9,4 +9,4 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+    S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
