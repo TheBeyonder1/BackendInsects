2345bf88dad9899b4367d6c70914e5dffc8c8a6e
Santiago Arce <santiago.arce@uao.edu.co>
2025-04-03 23:27:19 -0500

    FIX: S3

--- app/services/s3.py
+++ app/services/s3.py
@@ -1,21 +1,23 @@
 import boto3
-import os
-
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_S3_REGION")  # <-- cambia esto si usás AWS_REGION en .env
-)
-
-BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+from flask import current_app
 
 def subir_imagen_a_s3(archivo, nombre_destino):
+    # Crear cliente S3 usando la configuración de Flask
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=current_app.config['AWS_ACCESS_KEY'],
+        aws_secret_access_key=current_app.config['AWS_SECRET_KEY'],
+        region_name=current_app.config['AWS_REGION']
+    )
+
+    bucket_name = current_app.config['S3_BUCKET_NAME']
+
     s3.upload_fileobj(
         archivo,
-        BUCKET_NAME,
+        bucket_name,
         nombre_destino,
         ExtraArgs={'ACL': 'public-read', 'ContentType': archivo.content_type}
     )
-    url = f"https://{BUCKET_NAME}.s3.{os.getenv('AWS_REGION')}.amazonaws.com/{nombre_destino}"
+
+    url = f"https://{bucket_name}.s3.{current_app.config['AWS_REGION']}.amazonaws.com/{nombre_destino}"
     return url
