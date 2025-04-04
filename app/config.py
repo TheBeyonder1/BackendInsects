80e40ae6f1f1026d1b7fb4197e4144df37f796a7
Santiago Arce <santiago.arce@uao.edu.co>
2025-04-03 23:22:32 -0500

    FIX: ENV

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
-    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-    AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID") 
+    AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")  
     S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
     AWS_REGION = os.getenv("AWS_S3_REGION")
